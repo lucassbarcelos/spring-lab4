fec2b710d78d7ca8c3317d6f61dee0a682879b5c
lucassbarcelos <lucasbarcelos58@gmail.com>
2024-02-21 22:04:01 -0300

    feat: initial

---
+++ src/main/java/com/lucassbarcelos/springapilab4/SpringApiLab4Application.java
@@ -0,0 +1,13 @@
+package com.lucassbarcelos.springapilab4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiLab4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringApiLab4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/lucassbarcelos/springapilab4/SpringApiLab4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucassbarcelos.springapilab4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiLab4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
