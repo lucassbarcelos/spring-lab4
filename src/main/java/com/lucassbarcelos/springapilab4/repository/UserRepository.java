c5e6a891fbb7857a8c9f2c224cea246fb5d9bcf0
lucassbarcelos <lucasbarcelos58@gmail.com>
2024-04-03 21:25:56 -0300

    feat: add new example of a join

--- src/main/java/com/lucassbarcelos/springapilab4/repository/UserRepository.java
+++ src/main/java/com/lucassbarcelos/springapilab4/repository/UserRepository.java
@@ -19,4 +19,14 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     @Query("select u from User u where u.name like %?1%")
     public List<User> getByNameLike(String name);
+
+    public Optional<User> getByNameAndPassword(String name, String pasword);
+
+    @Query("select u from User u where u.name = ?1 and u.password = ?2")
+    public Optional<User> getByNameAndPasswordQuery(String name, String pasword);
+
+    public List<User> getByAuthorizationsName(String authName);
+
+    @Query("select u from User u join u.authorizations a where a.name = ?1")
+    public List<User> getByAuthorizationsNameQuery(String authName);
 }
